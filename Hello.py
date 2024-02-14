a0b688dce582ff3233ee9971197d5b7aefa7dd51
11KK12 <76783163+11KK12@users.noreply.github.com>
2024-02-14 12:18:21 +0000

    First version of ESEF RAG

--- Hello.py
+++ Hello.py
@@ -1,51 +1,52 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from utils import run_rag_pipeline
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+# Page setup
+st.set_page_config(
+        page_title="ESEF Reports Chat",
+        page_icon="📈",
     )
+st.title("ESEF Reports Chat")
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+# Initialize session state
+if "prompt_history" not in st.session_state:
+    st.session_state.prompt_history = """
+    <|im_start|>user
     """
-    )
+if "history" not in st.session_state:
+    st.session_state.history = []
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+# Show current chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-if __name__ == "__main__":
-    run()
+# Run RAG pipeline when user enters new question
+if prompt := st.chat_input("Ask a question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+
+        filter = "" #default: no filter; example: filter = "title eq 'finnair'"
+        k = 2 # number of search results to be included in the prompt
+        temperature = 0.2 # controls the randomness of the generation.
+
+        # load chat history from session state for multi-question conversations
+        history = st.session_state.history
+        prompt_history = st.session_state.prompt_history
+
+        answer, prompt_history, history = run_rag_pipeline(user_input=prompt, temperature=temperature, filter=filter, k=k, prompt_history=prompt_history, history=history)
+
+        # Save chat history in session state
+        st.session_state.history = history
+        st.session_state.prompt_history = prompt_history
+
+        message_placeholder.markdown(answer)
+
+    st.session_state.messages.append({"role": "assistant", "content": answer})
--- utils.py
+++ utils.py
@@ -1,28 +1,115 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import inspect
-import textwrap
+# Imports
+from azure.core.credentials import AzureKeyCredential
+import openai
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+from azure.search.documents import SearchClient
+from azure.search.documents.models import Vector 
 
 import streamlit as st
 
+# Set up openai
+openai.api_type = st.secrets["OPENAI_API_TYPE"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_base = st.secrets["OPENAI_API_BASE"]
+openai.api_version = st.secrets["OPENAI_API_VERSION"]
+gpt_engine_name = st.secrets["GPT_ENGINE_NAME"]
+embedding_engine_name = st.secrets["EMBEDDING_ENGINE_NAME"]
+search_index_name = "esef_reports_rag_index"
+search_endpoint = st.secrets["SEARCH_ENDPOINT"]
+search_key= st.secrets["SEARCH_KEY"]
 
-def show_code(demo):
-    """Showing the code of the demo."""
-    show_code = st.sidebar.checkbox("Show code", True)
-    if show_code:
-        # Showing the code of the demo.
-        st.markdown("## Code")
-        sourcelines, _ = inspect.getsourcelines(demo)
-        st.code(textwrap.dedent("".join(sourcelines[1:])))
+summary_prompt_template = """Alla on yhteenveto tähänastisesta keskustelusta ja käyttäjän esittämä uusi kysymys, johon on vastattava etsimällä tietopankista. Luo hakukysely keskustelun ja uuden kysymyksen perusteella. Lähteiden nimet eivät ole hyviä hakusanoja, jotka kannattaa sisällyttää hakukyselyyn.
+
+Yhteenveto:
+{summary}
+
+Kysymys:
+{question}
+
+Hakukysely:
+"""
+
+# Prompt Template Finnish
+prompt_prefix = """<|im_start|>system
+Assistentti auttaa rahoitusvalvojia löytämään tietoja rahoituslaitoksista. 
+Vastaa AINOASTAAN alla olevassa lähdeluettelossa luetelluilla tosiasioilla. Jos alla olevat tiedot eivät riitä, sano, ettet tiedä. Älä luo vastauksia, joissa ei käytetä alla olevia lähteitä. Jos selventävän kysymyksen esittäminen käyttäjälle auttaisi, esitä kysymys. 
+Jokaisessa lähteessä on nimi, jota seuraa kaksoispiste ja varsinainen tieto. Ilmoita aina lähteen nimi jokaisesta vastauksessa käyttämästäsi tiedosta. Käytä lähdeviittaukseen neliöjalkoja, esim. [info_1]. Älä yhdistä lähteitä, vaan mainitse jokainen lähde erikseen, esim. [info_1][info_2].
+
+Lähteet:
+{sources}
+
+<|im_end|>"""
+
+# RAG Preparation
+turn_prefix = """
+<|im_start|>user
+"""
+turn_suffix = """
+<|im_end|>
+<|im_start|>assistant
+"""
+prompt_history = turn_prefix
+history = []
+
+# Functions
+def search(query: str, filter="", k=5):
+    credential = AzureKeyCredential(search_key)
+    search_client = SearchClient(endpoint=search_endpoint, index_name=search_index_name, credential=credential)
+
+    vector = Vector(value=generate_embeddings(query, embedding_engine_name), k=3, fields="contentVector")
+
+    results = search_client.search(  
+        search_text=query,
+        vectors= [vector],
+        filter=filter,
+        select=["id", "title", "content"],
+        top=k
+    ) 
+
+    return results
+
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+# Function to generate embeddings for title and content fields, also used for query embeddings
+def generate_embeddings(text: str, embedding_engine_name: str) -> list:
+    response = openai.Embedding.create(
+        input=text, engine=embedding_engine_name)
+    embeddings = response['data'][0]['embedding'] # output dimensions = 1536
+    return embeddings
+
+def run_rag_pipeline(user_input: str, temperature: int, filter: str, k: int, prompt_history: str, history: list):
+
+    # Generating search query based on user input and chat history
+    if len(history) > 0:
+        completion = openai.Completion.create(
+            engine=gpt_engine_name,
+            prompt=summary_prompt_template.format(summary="\n".join(history), question=user_input),
+            temperature=temperature,
+            max_tokens=32,
+            stop=["\n"])
+        query = completion.choices[0].text
+    else:
+        query = user_input
+
+    # Searching for documents
+    r = search(query=query, filter=filter, k=k)
+    results = [doc["id"] + ": " + doc["content"].replace("\n", " ").replace("\r", " ") for doc in r]
+    content = "\n".join(results)
+
+    # Create prompt with content and context
+    prompt = prompt_prefix.format(sources=content) + prompt_history + user_input + turn_suffix
+
+    # Run prompt
+    completion = openai.Completion.create(
+        engine=gpt_engine_name, 
+        prompt=prompt, 
+        temperature=temperature, 
+        max_tokens=1024,
+        stop=["<|im_end|>", "<|im_start|>"])
+
+    # Update history
+    answer = completion.choices[0].text
+    prompt_history += user_input + turn_suffix + answer + "\n<|im_end|>" + turn_prefix
+    history.append("user: " + user_input)
+    history.append("assistant: " + completion.choices[0].text)
+
+    return answer, prompt_history, history
