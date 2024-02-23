169b5526490a1a461d5d27f07878c1c310206e75
11KK12 <76783163+11KK12@users.noreply.github.com>
2024-02-23 11:14:32 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -35,8 +35,7 @@ if st.session_state.messages == []:
 for message in st.session_state.messages:
     answer = message["answer"]
     with st.chat_message(answer["role"]):
-        #st.markdown(answer["content"])
-        st.write_stream(stream_text(answer["content"]))
+        st.markdown(answer["content"])
         for source in message["sources"]:
             with st.expander("Lue lähde  [" + source[0] + "]..."):
                 # ToDo: **highlight** source sentences in markdown? source retrieval with guardrails?
@@ -77,7 +76,8 @@ if prompt := st.chat_input("Kysy kysymys..."):
             # Save chat history in session state
             st.session_state.history = history
             st.session_state.prompt_history = prompt_history
-            message_placeholder.markdown(answer)
+            #message_placeholder.markdown(answer)
+            message_placeholder.write_stream(stream_text(answer))
             st.session_state.messages.append({"answer": {"role": "assistant", "content": answer}, "sources": sources})
 
             # Add source information
